fa48e6f1759b824e5809abae1808d5615e5d289b
amiacs <amiacs@gmail.com>
2023-02-06 12:01:16 +0100

    SCC pubsub subscribe

---
+++ SCC-Sentinel-Integration.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Copyright 2023 XXX
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import base64
+import functions_framework
+
+# Triggered from a message on SCC Cloud Pub/Sub topic.
+@functions_framework.cloud_event
+def scc_pubsub_subscribe(scc_event):
+    scc_finding = base64.b64decode(scc_event.data["message"]["data"]).decode()
+    print("SCC Finding: ", scc_finding)
