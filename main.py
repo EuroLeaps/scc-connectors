045de84646a3660397549daddbaba0f962ab2c10
amiacs <amiacs@gmail.com>
2023-03-19 23:40:28 +0100

    Support for GCP secret manager to retrieve Azure secrets

--- main.py
+++ main.py
@@ -21,14 +21,33 @@ import datetime
 import hashlib
 import hmac
 import os
+from google.cloud import secretmanager
 from dotenv import load_dotenv
 load_dotenv()
+
+PROJECT_ID = os.environ.get("PROJECT_ID", "")
+
+def get_secret_from_secret_manager(secret_id):
+    client = secretmanager.SecretManagerServiceClient()
+    resource_name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/latest"
+    try:
+        response = client.access_secret_version(resource_name)
+    except:
+        print(f"Error in retreiving secret: {secret_id}")
+    secret_value = response.payload.data.decode('UTF-8')
+    return secret_value
 
 # Update the customer ID to your Log Analytics workspace ID
 try:
     AZURE_LOG_ANALTYTICS_WORKSPACE_ID = os.environ["AZURE_LOG_ANALTYTICS_WORKSPACE_ID"]
 except KeyError:
     print("AZURE_LOG_ANALTYTICS_WORKSPACE_ID not found in env file.. Trying Secret Manager")
+    if(PROJECT_ID != ""):
+        AZURE_LOG_ANALTYTICS_WORKSPACE_ID = get_secret_from_secret_manager("AZURE_LOG_ANALTYTICS_WORKSPACE_ID")
+        print('ID ', AZURE_LOG_ANALTYTICS_WORKSPACE_ID)
+    else:
+        print("PROJECT_ID not found in env file.. Cannot use Secret Manager. Exiting..")
+        exit()
 
 # For the shared key, use either the primary or the secondary Connected Sources client authentication key 
 # If no name is provided as environment variable, then scc_table will be used as default
@@ -36,6 +55,12 @@ try:
     AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY = os.environ["AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY"]
 except KeyError:
     print("AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY not found in env file.. Trying Secret Manager")
+    if(PROJECT_ID != ""):
+        AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY = get_secret_from_secret_manager("AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY")
+        print('KEY ', AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY)
+    else:
+        print("PROJECT_ID not found in env file.. Cannot use Secret Manager. Exiting..")
+        exit()
 
 # The name of the Log Analytics custom table where SCC Alerts will be stored
 AZURE_LOG_ANALYTICS_CUSTOM_TABLE = os.environ.get("AZURE_LOG_ANALYTICS_CUSTOM_TABLE", "scc_table")
