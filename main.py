f3433133282353ece72e49f41eb5c3745ed287e5
Amit Verma <amiacs@gmail.com>
2023-03-22 14:19:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,14 +26,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PROJECT_ID = os.environ.get("PROJECT_ID", "")
+print('PROJECT_ID ', PROJECT_ID)
 
 def get_secret_from_secret_manager(secret_id):
     client = secretmanager.SecretManagerServiceClient()
     resource_name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/latest"
     try:
-        response = client.access_secret_version(resource_name)
-    except:
-        print(f"Error in retreiving secret: {secret_id}")
+        response = client.access_secret_version(request={"name": resource_name})
+    except Exception as e:
+        print(f"Error in retreiving secret: {secret_id}: ", e.message, e.args)
     secret_value = response.payload.data.decode('UTF-8')
     return secret_value
 
@@ -43,8 +44,9 @@ try:
 except KeyError:
     print("AZURE_LOG_ANALTYTICS_WORKSPACE_ID not found in env file.. Trying Secret Manager")
     if(PROJECT_ID != ""):
+        print('PROJECT_ID ', PROJECT_ID)
         AZURE_LOG_ANALTYTICS_WORKSPACE_ID = get_secret_from_secret_manager("AZURE_LOG_ANALTYTICS_WORKSPACE_ID")
-        print('ID ', AZURE_LOG_ANALTYTICS_WORKSPACE_ID)
+        print('Retrieved AZURE_LOG_ANALTYTICS_WORKSPACE_ID')
     else:
         print("PROJECT_ID not found in env file.. Cannot use Secret Manager. Exiting..")
         exit()
@@ -56,8 +58,9 @@ try:
 except KeyError:
     print("AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY not found in env file.. Trying Secret Manager")
     if(PROJECT_ID != ""):
+        print('PROJECT_ID ', PROJECT_ID)
         AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY = get_secret_from_secret_manager("AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY")
-        print('KEY ', AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY)
+        print('Retrieved AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY')
     else:
         print("PROJECT_ID not found in env file.. Cannot use Secret Manager. Exiting..")
         exit()
