d3457259e252e3b97d8afa97fdfdf9d04dc9be72
Amit Verma <amiacs@gmail.com>
2023-06-14 23:50:09 +0200

    Terraform support

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-# TODO enable APIs for pubsub, iam, scc, cloud resource manager, cloud functions, event arc, artifact registry, cloud run
-# cloud build
+locals {
+  gcp_organization = "YOUR_ORG_ID"
+  gcp_project = "YOUR_PROJ_ID"
+  azure_log_analytics_workspace_id = "YOUR_WORKSPACE_ID"
+  azure_log_analytics_authentication_key = "YOUR_KEY"
+  azure_log_analytics_custom_table = "scc_alerts_table"
+}
+  
 terraform {
   required_providers {
     google = {
@@ -9,20 +15,58 @@ terraform {
   }
 }
 
+resource "google_project_service" "pubsub_api" {
+  service = "pubsub.googleapis.com"
+}
+
+resource "google_project_service" "iam_api" {
+  service = "iam.googleapis.com"
+}
+
+resource "google_project_service" "scc_api" {
+  service = "securitycenter.googleapis.com"
+}
+
+resource "google_project_service" "resource_manager_api" {
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "cloud_functions_api" {
+  service = "cloudfunctions.googleapis.com"
+}
+
+resource "google_project_service" "eventarc_api" {
+  service = "eventarc.googleapis.com"
+}
+
+resource "google_project_service" "artifact_registry_api" {
+  service = "artifactregistry.googleapis.com"
+}
+
+resource "google_project_service" "cloud_run_api" {
+  service = "run.googleapis.com"
+}
+
+resource "google_project_service" "cloud_build_api" {
+  service = "cloudbuild.googleapis.com"
+}
+
 resource "google_pubsub_topic" "default" {
   name = "scc-findings-topic"
-  project = "scc-tf1"
+  depends_on = [google_project_service.pubsub_api]
 }
 
 resource "google_scc_notification_config" "default" {
   config_id    = "scc-findings-export"
-  organization = "723388619511"
+  organization = local.gcp_organization
   description  = "Continuous export of SCC findings to Azure Sentinel"
   pubsub_topic =  google_pubsub_topic.default.id
 
   streaming_config {
     filter = "state = \"ACTIVE\" AND NOT mute=\"MUTED\""
   }
+
+  depends_on = [google_project_service.scc_api]
 }
 
 resource "random_id" "bucket_prefix" {
@@ -32,24 +76,25 @@ resource "random_id" "bucket_prefix" {
 resource "google_service_account" "default" {
   account_id   = "scc-azure-connector-sa"
   display_name = "SCC Azure Connector Service Account"
+  depends_on = [google_project_service.iam_api]
 }
 
 resource "google_project_iam_member" "invoking" {
-  project = "scc-tf1"
+  project = local.gcp_project
   role    = "roles/run.invoker"
   member  = "serviceAccount:${google_service_account.default.email}"
   depends_on = [google_service_account.default]
 }
 
 resource "google_project_iam_member" "event-receiving" {
-  project = "scc-tf1"
+  project = local.gcp_project
   role    = "roles/eventarc.eventReceiver"
   member  = "serviceAccount:${google_service_account.default.email}"
   depends_on = [google_service_account.default]
 }
 
 resource "google_project_iam_member" "log-writer" {
-  project = "scc-tf1"
+  project = local.gcp_project
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.default.email}"
   depends_on = [google_service_account.default]
@@ -98,7 +143,9 @@ resource "google_cloudfunctions2_function" "default" {
     available_memory   = "256M"
     timeout_seconds    = 60
     environment_variables = {
-      AZURE_LOG_ANALYTICS_CUSTOM_TABLE = "scc_table"
+      AZURE_LOG_ANALTYTICS_WORKSPACE_ID = local.azure_log_analytics_workspace_id
+      AZURE_LOG_ANALYTICS_AUTHENTICATION_KEY = local.azure_log_analytics_authentication_key
+      AZURE_LOG_ANALYTICS_CUSTOM_TABLE = local.azure_log_analytics_custom_table
     }
     ingress_settings               = "ALLOW_INTERNAL_ONLY"
     all_traffic_on_latest_revision = true
@@ -119,5 +166,11 @@ resource "google_cloudfunctions2_function" "default" {
     google_project_iam_member.invoking,
     google_project_iam_member.event-receiving,
     google_project_iam_member.log-writer,
+    google_project_service.resource_manager_api,
+    google_project_service.cloud_functions_api,
+    google_project_service.eventarc_api,
+    google_project_service.artifact_registry_api,
+    google_project_service.cloud_run_api,
+    google_project_service.cloud_build_api,
     ]
 }
